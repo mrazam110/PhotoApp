2f1110e47a059f74c14548a254f60f32942b064c
Muhammad Raza Master <mrazam110@gmail.com>
2022-11-07 09:51:18 +0100

    Basic Structure of DummyPhotosListViewController with binding to DummyPhotosListViewModel

--- PhotoApp/Presentation/DummyPhoto List/Controller/DummyPhotosListViewController.swift
+++ PhotoApp/Presentation/DummyPhoto List/Controller/DummyPhotosListViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class DummyPhotosListViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // MARK: - Private Properties
+    
+    private var viewModel: DummyPhotosListViewModel
+    
+    // MARK: - Inits
+    
+    init(viewModel: DummyPhotosListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: "DummyPhotosListViewModel", bundle: .main)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view
+        viewModel.viewDidLoad()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupViews() {
+        navigationItem.title = viewModel.screenTitle
+    }
+    
+    private func  bindViewModel() {
+        viewModel.reloadData = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
     }
 }
+
+// MARK: - UITableViewDelegate and UITableViewDataSource
